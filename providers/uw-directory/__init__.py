91f505f15759a9cca3942ae73766c86a9fa56bb2
Finn <finn@finn.io>
2016-04-07 09:27:20 -0700

    If ldap3 can't be imported, don't run

--- providers/uw-directory/__init__.py
+++ providers/uw-directory/__init__.py
@@ -1,17 +1,23 @@
 """Search the UW Directory."""
-import ldap3
+try:
+    import ldap3
+    conn = ldap3.Connection('directory.washington.edu', auto_bind=True)
+    ready = True
+except ImportError:
+    ready = False
 
-conn = ldap3.Connection('directory.washington.edu', auto_bind=True)
 BASE_DN = 'ou=People,o=University of Washington,c=US'
 
 
 def lookup(number):
     """Look for any entires in the UW directory with the given number."""
+    if not ready:
+        return None
     number = "+%s %s %s-%s" % (number[0], number[1:4], number[4:7], number[7:11])
     query = "(|(telephoneNumber=%s)(homePhone=%s))" % (number, number)
-    conn.search(BASE_DN,  query, attributes=['cn'])
+    conn.search(BASE_DN,  query, attributes=['cn', 'mail'])
     if len(conn.entries) > 0:
-        return conn.entries[0]['cn']
+        return "%s <%s>" % (conn.entries[0]['cn'], conn.entries[0]['mail'])
     else:
         return None
 
